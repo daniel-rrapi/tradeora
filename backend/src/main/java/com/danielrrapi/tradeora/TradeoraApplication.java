be634ad84bd9f47dbdc2530a3c0a600f91b68287
Daniel Rrapi <danielrrapi@proton.me>
2024-08-13 09:41:04 +0200

    First commit

---
+++ backend/src/main/java/com/danielrrapi/tradeora/TradeoraApplication.java
@@ -0,0 +1,13 @@
+package com.danielrrapi.tradeora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeoraApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/danielrrapi/tradeora/TradeoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielrrapi.tradeora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
