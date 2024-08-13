c7fa529526aa481d0809567e0b5ba78f3ea435ab
Daniel Rrapi <danielrrapi@proton.me>
2024-08-13 23:00:44 +0200

    Created basic classes

---
+++ backend/src/main/java/com/danielrrapi/tradeora/entities/Item.java
@@ -0,0 +1,57 @@
+package com.danielrrapi.tradeora.entities;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "items")
+@Entity
+public class Item {
+
+    @Id
+    @Setter(AccessLevel.NONE)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+
+    private String category;
+
+    private String description;
+
+    private double price;
+
+    private String location;
+
+    private boolean is_shippable;
+
+    private boolean is_available;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    public Item(String name, String category, String description, double price, String location, boolean is_shippable, boolean is_available) {
+        this.name = name;
+        this.category = category;
+        this.description = description;
+        this.price = price;
+        this.location = location;
+        this.is_shippable = is_shippable;
+        this.is_available = is_available;
+    }
+}
---
+++ backend/src/main/java/com/danielrrapi/tradeora/entities/User.java
@@ -0,0 +1,100 @@
+package com.danielrrapi.tradeora.entities;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "users")
+@Entity
+public class User implements UserDetails {
+
+    @Id
+    @Setter(AccessLevel.NONE)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    private String email;
+
+    private String password;
+
+    private LocalDate dob;
+
+    @Column(name = "mailing_list")
+    private boolean mailingList;
+
+    @Column(name = "phone_number")
+    private int phoneNumber;
+
+    private String role;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updated_at;
+
+    public User(String firstName, String lastName, String email, String password, LocalDate dob, boolean mailingList, int phoneNumber, String role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.dob = dob;
+        this.mailingList = mailingList;
+        this.phoneNumber = phoneNumber;
+        this.role = role;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
