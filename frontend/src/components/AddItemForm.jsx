fccb1f50df02b76ff3d07e1f6faa38f3fb08f367
daniel-rrapi <danielrrapi@proton.me>
2024-10-26 17:16:49 +0200

    Style: styling AddItemForm component

--- frontend/src/components/AddItemForm.jsx
+++ frontend/src/components/AddItemForm.jsx
@@ -1,5 +1,5 @@
 export function AddItemForm() {
-    return <form className="container mx-auto md:w-2/3">
+    return <form className="container mx-auto md:w-2/3 grid grid-cols-1 sm:grid-cols-2 gap-5 px-10 sm:px-0">
         <label>Category</label>
         <select>
             <option>Select a category</option>
@@ -21,8 +21,10 @@ export function AddItemForm() {
         <input type="text"/>
 
         <label>Shipping</label>
-        <input type="checkbox"/>
-        <p>Available for shipping</p>
+        <div className="flex gap-3">
+            <input type="checkbox"/>
+            <p>Available for shipping</p>
+        </div>
     </form>
 
 
